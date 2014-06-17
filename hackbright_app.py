972d491302ba19cb77aa616cf649a6e51da61057
Hackbright Student <info@hackbrightacademy.com>
2014-06-16 18:06:17 -0700

    end of Monday commit

--- hackbright_app.py
+++ hackbright_app.py
@@ -3,6 +3,12 @@ import sqlite3
 DB = None
 CONN = None
 
+def make_new_student(first_name, last_name, github):
+    query = """INSERT into Students values (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    print "Succesfully added student: %s %s" % (first_name, last_name)
+
 def get_student_by_github(github):
     query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
     DB.execute(query, (github,))
@@ -11,11 +17,25 @@ def get_student_by_github(github):
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
+def get_project_by_title(title):
+    query = """SELECT title, description FROM Projects WHERE title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    print """\
+Title: %s
+Description: %s""" % (row[0], row[1])
+
+def add_project(title, description, max_grade):
+    query = """INSERT into Projects values (?, ?, ?)"""
+    DB.execute(query, (title, description, max_grade))
+    CONN.commit()
+    print "Succesfully added new project: %s %s %r" % (title, description, max_grade)
+
+
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("hackbright.db")
     DB = CONN.cursor()
-
 def main():
     connect_to_db()
     command = None
@@ -29,6 +49,10 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "get_project":
+            get_project_by_title(*args)
+        elif command == "add_project":
+            add_project(*args)
 
     CONN.close()
 
